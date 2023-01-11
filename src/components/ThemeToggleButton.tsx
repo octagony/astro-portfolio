26687192798f175241a1bec9f99aba28d8da19a4
Octagony <moctagony@gmail.com>
2023-01-11 20:32:16 +0300

    [DEVELOP] Fix theme toggler

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,51 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { FaMoon, FaSun } from 'react-icons/fa'
+import React, { useEffect, useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const ThemeToggle = () => {
-  const [isMounted, setIsMounted] = useState(false)
+  const [isMounted, setIsMounted] = useState(false);
   const [theme, setTheme] = useState(() => {
     if (import.meta.env.SSR) {
-      return undefined
+      return undefined;
     }
-    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-      return localStorage.getItem('theme')
+    if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+      return localStorage.getItem("theme");
     }
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark'
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
     }
-    return 'light'
-  })
+    return "light";
+  });
 
   const toggleTheme = () => {
-    const t = theme === 'light' ? ' dark' : 'light'
-    localStorage.setItem('theme', t)
-    setTheme(t)
-  }
+    const t = theme === "light" ? " dark" : "light";
+    localStorage.setItem("theme", t);
+    setTheme(t);
+  };
 
   useEffect(() => {
-    const root = document.documentElement
-    if (theme === 'light') {
-      root.classList.remove('dark')
+    const root = document.documentElement;
+    if (theme === "light") {
+      root.classList.remove("dark");
     } else {
-      root.classList.add('dark')
+      root.classList.add("dark");
     }
-  }, [theme])
+  }, [theme]);
 
   useEffect(() => {
-    setIsMounted(true)
-  }, [])
+    setIsMounted(true);
+  }, []);
 
   return isMounted ? (
     <div
-      className="absolute bottom-7 right-7 p-2 cursor-pointer transition-opacity"
+      className="fixed bottom-7 right-7 p-2 text-white cursor-pointer rounded-full bg-indigo-500 transition-opacity dark:text-white dark:bg-pink-500"
       onClick={toggleTheme}
       aria-label="Toggle Theme"
     >
-      {theme === 'light' ? <FaMoon size={25} /> : <FaSun size={25} />}
+      {theme === "light" ? <FaMoon size={25} /> : <FaSun size={25} />}
     </div>
   ) : (
     <div />
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
