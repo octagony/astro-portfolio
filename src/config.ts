f32ddcb8558ab113f230715bdd65cb0371ff9a8b
octagony <moctagony@gmail.com>
2023-03-29 20:45:15 +0300

    [FEAT] Added favicons

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Rutskiy Portfolio";
-export const SITE_DESCRIPTION = "Mikhail Rutskiy Portfolio Site";
-export const HOMEPAGE_URL = "https://github.com/octagony";
-export const NVIM_CONFIG = HOMEPAGE_URL + "/dev_files";
-export const DWM_CONFIG = HOMEPAGE_URL + "/dwm-config-files";
+export const SITE_TITLE = 'Rutskiy Portfolio'
+export const SITE_DESCRIPTION = 'Mikhail Rutskiy Portfolio Site'
+export const HOMEPAGE_URL = 'https://github.com/octagony'
+export const NVIM_CONFIG = HOMEPAGE_URL + '/dev_files'
+export const DWM_CONFIG = HOMEPAGE_URL + '/dwm-config-files'
+export const TELEGRAM_LINK = 'https://t.me/tnotu'
+export const MAIL_LINK = 'mailto:moctagony@gmail.com'
+export const LINKEDIN_LINK =
+	'https://www.linkedin.com/in/mihail-ruckiy-7a309323a/'
