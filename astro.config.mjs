d9fb2c4e6423bce617ee0c82dcafdcbd6aaf39af
octagony <moctagony@gmail.com>
2023-09-04 20:51:22 +0300

    [FEAT] Switched to font Hind

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,24 +12,19 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/octagony",
-  integrations: [
-    mdx(),
-    sitemap(),
-    react(),
-    tailwind(),
-  ],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [
       [
         addClasses,
         {
-          h1: "text-4xl font-bold font-heebo",
-          h2: "text-2xl font-bold font-heebo",
-          h3: "text-xl font-bold font-heebo",
-          h4: "text-lg font-bold font-heebo",
-          h5: "font-bold font-heebo",
-          h6: "font-bold font-heebo",
+          h1: "text-4xl font-bold font-hind",
+          h2: "text-2xl font-bold font-hind",
+          h3: "text-xl font-bold font-hind",
+          h4: "text-lg font-bold font-hind",
+          h5: "font-bold font-hind",
+          h6: "font-bold font-hind",
           img: "border border-slate-300 dark:border-neutral-700 rounded-xl mb-6",
           a: "underline underline-offset-2 hover:text-pink-500 decoration-pink-500",
         },
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,62 +1,63 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				heebo: ['Heebo', 'Verdana', 'sans-serif'],
-			},
-			fontSize: {
-				xs: '0.82rem',
-				sm: '0.98rem',
-				base: '1.15rem',
-				lg: '1.22rem',
-				xl: '1.36rem',
-				'1.5xl': '1.5rem',
-				'2xl': '1.725rem',
-				'3xl': '2.155rem',
-				'4xl': '2.58rem',
-				'5xl': '3.45rem',
-				'6xl': '4.3rem',
-				'7xl': '5.17rem',
-				'8xl': '6.9rem',
-				'9xl': '9.2rem',
-			},
-			keyframes: {
-				scroll: {
-					'0%': { left: '0%' },
-					'100%': { left: '-100%' },
-				},
-				scroll_reverse: {
-					'0%': { left: '-100%' },
-					'100%': { left: '0' },
-				},
-				spin_logo: {
-					'0%': { transform: 'rotate(0deg)' },
-					'100%': { transform: 'rotate(360deg)' },
-				},
-				scaleIn: {
-					'0%': {
-						opacity: 0,
-						transform: 'scale(0.9)',
-					},
-					'50%': {
-						opacity: 0.3,
-					},
-					'100%': {
-						opacity: 1,
-						transform: 'scale(1)',
-					},
-				},
-			},
-			animation: {
-				scroll: 'scroll 10s linear 0s infinite normal',
-				scroll_reverse: 'scroll_reverse 10s linear 0s infinite normal',
-				spin_logo: 'spin_logo 25s linear 0s infinite normal',
-				scaleIn: 'scaleIn .35s ease-in-out',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        heebo: ["Heebo", "Verdana", "sans-serif"],
+        hind: ["Hind", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.82rem",
+        sm: "0.98rem",
+        base: "1.15rem",
+        lg: "1.22rem",
+        xl: "1.36rem",
+        "1.5xl": "1.5rem",
+        "2xl": "1.725rem",
+        "3xl": "2.155rem",
+        "4xl": "2.58rem",
+        "5xl": "3.45rem",
+        "6xl": "4.3rem",
+        "7xl": "5.17rem",
+        "8xl": "6.9rem",
+        "9xl": "9.2rem",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { left: "0%" },
+          "100%": { left: "-100%" },
+        },
+        scroll_reverse: {
+          "0%": { left: "-100%" },
+          "100%": { left: "0" },
+        },
+        spin_logo: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        scaleIn: {
+          "0%": {
+            opacity: 0,
+            transform: "scale(0.9)",
+          },
+          "50%": {
+            opacity: 0.3,
+          },
+          "100%": {
+            opacity: 1,
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        scroll: "scroll 10s linear 0s infinite normal",
+        scroll_reverse: "scroll_reverse 10s linear 0s infinite normal",
+        spin_logo: "spin_logo 25s linear 0s infinite normal",
+        scaleIn: "scaleIn .35s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
